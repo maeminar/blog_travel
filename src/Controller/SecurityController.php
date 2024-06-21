bd8d02b24336b27e95da8481ecc5b6f43837861e
Emma <emmaminary@gmail.com>
2024-06-21 14:59:18 +0200

    message flash

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,6 +18,10 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        if (!$error && $this->getUser()) {
+            $this->addFlash('success', 'Vous êtes connecté avec succès !');
+        }
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
